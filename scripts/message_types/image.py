1fc6b29699f7f336c990489691437abc39b59602
Jean-Michel Fortin <jmfortin29@gmail.com>
2024-02-29 16:35:13 -0500

    added image rectification

--- scripts/message_types/image.py
+++ scripts/message_types/image.py
@@ -29,9 +29,19 @@ ENCODINGS = {  # COMMENTED ENCODINGS ARE NOT TESTED
 }
 
 
-def extract_images_from_rosbag(bag_file, topic_name, output_folder, image_ext="png"):
-
+def extract_images_from_rosbag(bag_file, topic_name, output_folder, image_ext="png", rectify=False):
+        
     with AnyReader([Path(bag_file)]) as reader:
+        if rectify:
+            # extract camera calibration matrix
+            camera_info_topic = "/".join(topic_name.split('/')[:-1] + ["camera_info"])  
+            connections = [x for x in reader.connections if x.topic == camera_info_topic] 
+            for connection, _, rawdata in reader.messages(connections=connections):
+                camera_info = reader.deserialize(rawdata, connection.msgtype)
+                camera_info_K = np.array(camera_info.k).reshape([3, 3])
+                camera_info_D = np.array(camera_info.d)
+                break    
+
         # iterate over messages
         print(f"Extracting images from topic \"{topic_name}\" to folder \"{output_folder.split('/')[-1]}\"")
         connections = [x for x in reader.connections if x.topic == topic_name]
@@ -41,10 +51,31 @@ def extract_images_from_rosbag(bag_file, topic_name, output_folder, image_ext="p
             np_image = image_to_numpy(msg)
             if "bgr" in msg.encoding:
                 np_image = cv2.cvtColor(np_image, cv2.COLOR_RGB2BGR)
+            if rectify:
+                np_image = cv2.undistort(np_image, camera_info_K, camera_info_D)  
             cv2.imwrite(os.path.join(output_folder, f"{int(timestamp):d}.{image_ext}"), np_image)
 
     print(f"Done ! Exported images to {output_folder}")
+
+
+# def extract_rectified_images_from_rosbag(bag_file, topic_name, output_folder, image_ext="png"):
         
+#         with AnyReader([Path(bag_file)]) as reader:
+#             # iterate over messages
+#             print(f"Extracting images from topic \"{topic_name}\" to folder \"{output_folder.split('/')[-1]}\"")
+#             connections = [x for x in reader.connections if x.topic == topic_name]
+#             for connection, _, rawdata in tqdm(reader.messages(connections=connections)):
+#                 msg = reader.deserialize(rawdata, connection.msgtype)
+#                 timestamp = msg.header.stamp.sec * 1e9 + msg.header.stamp.nanosec  
+#                 np_image = image_to_numpy(msg)
+#                 if "bgr" in msg.encoding:
+#                     np_image = cv2.cvtColor(np_image, cv2.COLOR_RGB2BGR)
+#                 rectified_image = rectify_image(np_image
+#                 cv2.imwrite(os.path.join(output_folder, f"{int(timestamp):d}.{image_ext}"), np_image)
+    
+#         print(f"Done ! Exported images to {output_folder}")
+        
+# def rectify_image(image, map1, map2):
         
 def sort_bracket_images(bag_file, metadata_topic, images_folder, bracketing_values, image_ext="png"):
     
