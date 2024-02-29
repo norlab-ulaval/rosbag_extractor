cf619b0f3e6ca59d9c13e9369cf7ff989c16b3f0
Jean-Michel Fortin <jmfortin29@gmail.com>
2024-02-29 16:34:18 -0500

    added pose extraction from odom messages

--- scripts/extract_data_from_rosbag.py
+++ scripts/extract_data_from_rosbag.py
@@ -8,23 +8,46 @@ from message_types.image import extract_images_from_rosbag, sort_bracket_images
 from message_types.imu import extract_imu_from_rosbag
 from message_types.gnss import extract_gnss_from_rosbag
 from message_types.audio import extract_audio_from_rosbag
+from message_types.odom import extract_odom_from_rosbag
 
 ################## PARAMETERS ##################
 
-INPUT_BAG = "/home/jean-michel/ros/bags/tp3_robmob/processed_bag_with_pose"
-OUTPUT_FOLDER = "/home/jean-michel/ros/bags/tp3_robmob/export"
+INPUT_BAG = "/home/jean-michel/ros/bags/active_probing_parking/merged_2024-02-18_15-01-58_processed"
+OUTPUT_FOLDER = "/home/jean-michel/ros/bags/active_probing_parking/merged_2024-02-18_15-01-58_extracted"
 BRACKETING_VALUES = np.array([1.0, 2.0, 4.0, 8.0, 16.0, 32.0])
 IMAGE_EXT = "png"
 TOPICS_TO_EXPORT = [
     {
-        "topic": "/lslidar_point_cloud",
-        "folder": "lidar",
-        "type": "point_cloud"
+        "topic": "/icp_odom",
+        "filename": "icp_odom.csv",
+        "type": "odometry"
+    },
+    {
+        "topic": "/odom_roboclaw",
+        "filename": "wheel_odom.csv",
+        "type": "odometry"
+    },
+    {
+        "topic": "/imu_and_wheel_odom",
+        "filename": "imu_and_wheel_odom.csv",
+        "type": "odometry"
     },
     # {
-    #     "topic": "/zed2i/zed_node/right_raw/image_raw_color",
+    #     "topic": "/zed_node/left_raw/image_raw_color",
+    #     "calibration": "/zed_node/left_raw/camera_info",
+    #     "folder": "camera_left",
+    #     "type": "rectified_image"
+    # },
+    # {
+    #     "topic": "/zed_node/right_raw/image_raw_color",
+    #     "calibration": "/zed_node/right_raw/camera_info",
     #     "folder": "camera_right",
-    #     "type": "image"
+    #     "type": "rectified_image"
+    # },
+    # {
+    #     "topic": "/lslidar_point_cloud",
+    #     "folder": "lidar",
+    #     "type": "point_cloud"
     # },
     # {
     #     "topic": "/audio/audio_stamped",
@@ -70,7 +93,6 @@ def check_output_folder(output_folder):
     if not os.path.exists(output_folder): 
         os.makedirs(output_folder)
 
-
 def check_requested_topics(bag_file, requested_topics):
     with AnyReader([Path(bag_file)]) as reader:
         bag_topics = set([x.topic for x in reader.connections])
@@ -80,7 +102,7 @@ def check_requested_topics(bag_file, requested_topics):
             raise ValueError(f"Topic {topic} not found in bag file, available topics are {bag_topics}")
 
 def main():
-    
+
     check_requested_topics(INPUT_BAG, [data['topic'] for data in TOPICS_TO_EXPORT])
     check_output_folder(OUTPUT_FOLDER)
     
@@ -92,7 +114,12 @@ def main():
             extract_images_from_rosbag(INPUT_BAG, data['topic'], output_folder, IMAGE_EXT)
             sort_bracket_images(INPUT_BAG, data['metadata_topic'], output_folder, BRACKETING_VALUES, IMAGE_EXT)
 
-        elif data['type'] == "image":
+        elif data['type'] == "rectified_image":
+            output_folder = os.path.join(OUTPUT_FOLDER, data['folder'])
+            os.makedirs(output_folder)
+            extract_images_from_rosbag(INPUT_BAG, data['topic'], output_folder, IMAGE_EXT, rectify=True)
+
+        elif data['type'] == "raw_image":
             output_folder = os.path.join(OUTPUT_FOLDER, data['folder'])
             os.makedirs(output_folder)
             extract_images_from_rosbag(INPUT_BAG, data['topic'], output_folder, IMAGE_EXT)
@@ -112,6 +139,10 @@ def main():
         elif data['type'] == "point_cloud":
             output_folder = os.path.join(OUTPUT_FOLDER, data['folder'])
             extract_point_clouds_from_rosbag(INPUT_BAG, data['topic'], output_folder)
+
+        elif data['type'] == "odometry":
+            output_file = os.path.join(OUTPUT_FOLDER, data['filename'])
+            extract_odom_from_rosbag(INPUT_BAG, data['topic'], output_file)
 
 
 if __name__ == "__main__":
---
+++ scripts/message_types/odom.py
@@ -0,0 +1,53 @@
+import pandas as pd
+from pathlib import Path
+from tqdm import tqdm
+from rosbags.highlevel import AnyReader
+import math
+
+
+## NOTE: THIS ONLY EXTRACTS THE POSE IN THE ODOMETRY MESSAGE, NOT THE COVARIANCE NOR THE TWIST
+
+def extract_odom_from_rosbag(bag_file, topic_name, output_file):
+        
+    odom_data = []
+
+    with AnyReader([Path(bag_file)]) as reader:
+        # iterate over messages
+        print(f"Extracting odometry data from topic \"{topic_name}\" to file \"{output_file.split('/')[-1]}\"")
+        connections = [x for x in reader.connections if x.topic == topic_name]
+        for connection, _, rawdata in tqdm(reader.messages(connections=connections)):
+            msg = reader.deserialize(rawdata, connection.msgtype)
+            quat = msg.pose.pose.orientation
+            roll, pitch, yaw = euler_from_quaternion(quat.x, quat.y, quat.z, quat.w)
+            odom_data.append([
+                int(msg.header.stamp.sec * 1e9 + msg.header.stamp.nanosec), 
+                msg.pose.pose.position.x,
+                msg.pose.pose.position.y,
+                msg.pose.pose.position.z,
+                roll,
+                pitch,
+                yaw
+            ])
+            
+        odom_df = pd.DataFrame(odom_data, columns=["timestamp", "x", "y", "z", "roll", "pitch", "yaw"])
+        odom_df.to_csv(output_file, index=False)
+
+    print(f"Done ! Exported odometry to {output_file}")
+
+
+def euler_from_quaternion(x, y, z, w):
+        
+        t0 = +2.0 * (w * x + y * z)
+        t1 = +1.0 - 2.0 * (x * x + y * y)
+        roll_x = math.atan2(t0, t1)
+     
+        t2 = +2.0 * (w * y - z * x)
+        t2 = +1.0 if t2 > +1.0 else t2
+        t2 = -1.0 if t2 < -1.0 else t2
+        pitch_y = math.asin(t2)
+     
+        t3 = +2.0 * (w * z + x * y)
+        t4 = +1.0 - 2.0 * (y * y + z * z)
+        yaw_z = math.atan2(t3, t4)
+     
+        return roll_x, pitch_y, yaw_z # in radians
