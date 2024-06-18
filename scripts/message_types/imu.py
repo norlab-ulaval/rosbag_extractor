063977fe806894f9cbe72ae646a18c96bd846deb
Jean-Michel Fortin <jmfortin29@gmail.com>
2024-06-18 17:00:24 -0400

    added orientation to imu data

--- scripts/message_types/imu.py
+++ scripts/message_types/imu.py
@@ -2,6 +2,7 @@ import pandas as pd
 from pathlib import Path
 from tqdm import tqdm
 from rosbags.highlevel import AnyReader
+import math
 
 
 def extract_imu_from_rosbag(bag_file, topic_name, output_file):
@@ -14,9 +15,13 @@ def extract_imu_from_rosbag(bag_file, topic_name, output_file):
         connections = [x for x in reader.connections if x.topic == topic_name]
         for connection, ros_time, rawdata in tqdm(reader.messages(connections=connections)):
             msg = reader.deserialize(rawdata, connection.msgtype)
+            roll, pitch, yaw = euler_from_quaternion(msg.orientation.x, msg.orientation.y, msg.orientation.z, msg.orientation.w)
             imu_data.append([
                 int(msg.header.stamp.sec * 1e9 + msg.header.stamp.nanosec), 
                 ros_time,
+                roll,
+                pitch,
+                yaw,
                 msg.angular_velocity.x, 
                 msg.angular_velocity.y, 
                 msg.angular_velocity.z, 
@@ -25,7 +30,25 @@ def extract_imu_from_rosbag(bag_file, topic_name, output_file):
                 msg.linear_acceleration.z
             ])
             
-        imu_df = pd.DataFrame(imu_data, columns=["timestamp", "ros_time", "gyro_x", "gyro_y", "gyro_z", "acc_x", "acc_y", "acc_z"])
+        imu_df = pd.DataFrame(imu_data, columns=["timestamp", "ros_time", "roll", "pitch", "yaw", "gyro_x", "gyro_y", "gyro_z", "acc_x", "acc_y", "acc_z"])
         imu_df.to_csv(output_file, index=False)
 
     print(f"Done ! Exported images to {output_file}")
+
+
+def euler_from_quaternion(x, y, z, w):
+        
+        t0 = +2.0 * (w * x + y * z)
+        t1 = +1.0 - 2.0 * (x * x + y * y)
+        roll_x = math.atan2(t0, t1)
+     
+        t2 = +2.0 * (w * y - z * x)
+        t2 = +1.0 if t2 > +1.0 else t2
+        t2 = -1.0 if t2 < -1.0 else t2
+        pitch_y = math.asin(t2)
+     
+        t3 = +2.0 * (w * z + x * y)
+        t4 = +1.0 - 2.0 * (y * y + z * z)
+        yaw_z = math.atan2(t3, t4)
+     
+        return roll_x, pitch_y, yaw_z # in radians
--- scripts/rosbag_to_directory.py
+++ scripts/rosbag_to_directory.py
@@ -5,9 +5,9 @@ from rosbag_extractor import RosbagExtractor
 ################## PARAMETERS ##################
 
 # INPUT_BAG = "/run/user/1000/gvfs/afp-volume:host=bucheron.local,user=norlab_admin,volume=home/olivier_gamache/dataset/forest_04-20-2023/bagfiles/backpack_2023-04-20-09-29-14.bag"
-INPUT_BAG = "/media/jean-michel/SSD_JM/Data/UL_20240416/merged_2024-04-16_10-04-45"
-OUTPUT_FOLDER = "/media/jean-michel/SSD_JM/Data/UL_20240416/exported_2024-04-16_10-04-45"
-CONFIG_FILE = os.path.join(os.path.dirname(__file__), "../configs/config_marmotte.json")
+INPUT_BAG = "/media/jean-michel/ssd_NS/jm/trav_2024-06-18_15-30-33"
+OUTPUT_FOLDER = "/home/jean-michel/Desktop/calib_warthog"
+CONFIG_FILE = os.path.join(os.path.dirname(__file__), "../configs/config_warthog.json")
 
 ################################################
 
