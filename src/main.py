e5872b18e857191650d1865f926c1e4dbec2d5f8
Jean-Michel Fortin <jmfortin29@gmail.com>
2026-02-03 19:01:38 +0000

    added silent option

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@
 
 import argparse
 import os
+import sys
 import yaml
 from src.rosbag_extractor import RosbagExtractor
 
@@ -20,12 +21,18 @@ def parse_args():
     parser.add_argument("-o", "--output", type=str, help="Output directory.", required=True)
     parser.add_argument("--ignore-missing", action="store_true", help="Ignore missing topics in the config file.")
     parser.add_argument("--overwrite", action="store_true", help="Overwrite existing files in the output directory.")
+    parser.add_argument("--silent", action="store_true", help="Silent mode - suppress all output to terminal.")
     return parser.parse_args()
 
 
 def main():
     args = parse_args()
     config = load_config(args.config)
+    
+    if args.silent:
+        sys.stdout = open(os.devnull, 'w')
+        sys.stderr = open(os.devnull, 'w')
+    
     rosbag_extractor = RosbagExtractor(args.input, config)
     rosbag_extractor.extract_data(args.output, overwrite=args.overwrite, ignore_missing=args.ignore_missing)
 
